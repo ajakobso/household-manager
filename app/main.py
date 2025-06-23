82f3ec4163ea8f48b65b5b34d5fdd6e972e4833c
ajakobso <ajakobso@g.jct.ac.il>
2025-06-23 12:40:47 +0300

    feat: change id to be uuid format and err flow inside if

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from schemas.tasks_schema import BaseTask, Status
 from datetime import datetime
 from typing import Annotated
 from json import loads
+from uuid import uuid4, UUID
 
 app = FastAPI()
 
@@ -16,17 +17,21 @@ async def root():
     ) 
 
 @app.get("/tasks/{task_id}")
-async def read_task(task_id: int):  # 
-    if task_id in tasks:
-        return tasks[task_id]
-    raise HTTPException(
+async def read_task(task_id: UUID):
+    if task_id not in tasks:
+        raise HTTPException(
         status_code=404,
         detail=f"Task with ID {task_id} Not Found"
         )
+    raise HTTPException(
+        status_code=200,
+        detail={"task": tasks[task_id]}
+    ) 
+   
 
 @app.post("/tasks/")
 async def create_task(task: BaseTask):
-    new_task_id: int = len(tasks) + 1
+    new_task_id = uuid4()
     new_task = {
         "task_id": new_task_id,
         "title": task.title,
@@ -42,48 +47,48 @@ async def create_task(task: BaseTask):
     tasks[new_task_id] = new_task
     raise HTTPException(
         status_code=200,
-        detail={"message": "Task Successfully Created:)", "task_id": new_task_id, "task": new_task}
+        detail={"message": "Task Successfully Created:)", "task": new_task}
     )
 
 @app.put("/tasks/{task_id}")
-async def update_task(task_id: int, task: BaseTask):
-    if task_id in tasks:
-        task_values = loads(tasks[task_id])
-        if task.status == Status.DONE:
-            updated_task = {
-                "task_id": task_values["task_id"],
-                "title": task.title or task_values["title"],
-                "description": task.description or task_values["description"],            
-                "status": task.status or task_values["status"],            
-                "priority": task.priority or task_values["priority"],            
-                "category": task.category or task_values["category"],                 
-                "due_date": task.due_date or task_values["due_date"],
-                "created_at": task_values["created_at"],
-                "completed_at": datetime.now().date(),                           
-                "partner_id": task.partner_id or task_values["partner_id"]            
-            }
-        else:
-            updated_task = {
-                "task_id": task_values["task_id"],
-                "title": task.title or task_values["title"],
-                "description": task.description or task_values["description"],            
-                "status": task.status or task_values["status"],            
-                "priority": task.priority or task_values["priority"],            
-                "category": task.category or task_values["category"],                 
-                "due_date": task.due_date or task_values["due_date"],
-                "created_at": task_values["created_at"],
-                "completed_at": None,                           
-                "partner_id": task.partner_id or task_values["partner_id"]
-            }
-        tasks[task_id] = updated_task
+async def update_task(task_id: UUID, task: BaseTask):
+    if task_id not in tasks:
         raise HTTPException(
-            status_code=200,
-            detail= {"message": "Successfully updated task {task_id}", "new_task": updated_task}
-        )
-    raise HTTPException(
         status_code=404,
-        detail="Unable to update non-existing task"
+        detail=f"Task with ID {task_id} Not Found"
         )
+    task_values = loads(tasks[task_id])
+    if task.status == Status.DONE: # Handle complete task status update
+        updated_task = {
+            "task_id": task_values["task_id"],
+            "title": task.title or task_values["title"],
+            "description": task.description or task_values["description"],            
+            "status": task.status or task_values["status"],            
+            "priority": task.priority or task_values["priority"],            
+            "category": task.category or task_values["category"],                 
+            "due_date": task.due_date or task_values["due_date"],
+            "created_at": task_values["created_at"],
+            "completed_at": datetime.now().date(),                           
+            "partner_id": task.partner_id or task_values["partner_id"]            
+        }
+    else:
+        updated_task = {
+            "task_id": task_values["task_id"],
+            "title": task.title or task_values["title"],
+            "description": task.description or task_values["description"],            
+            "status": task.status or task_values["status"],            
+            "priority": task.priority or task_values["priority"],            
+            "category": task.category or task_values["category"],                 
+            "due_date": task.due_date or task_values["due_date"],
+            "created_at": task_values["created_at"],
+            "completed_at": None,                           
+            "partner_id": task.partner_id or task_values["partner_id"]
+        }
+    tasks[task_id] = updated_task
+    raise HTTPException(
+        status_code=200,
+        detail= {"message": "Successfully updated task {task_id}", "new_task": updated_task}
+    )
 
 @app.get('/tasks/')
 async def get_tasks():
@@ -93,16 +98,18 @@ async def get_tasks():
     )
 
 @app.delete('/tasks/{task_id}')
-async def delete_task(task_id: int):
-    if task_id in tasks:
-        del tasks[task_id]
+async def delete_task(task_id: UUID):
+    if task_id not in tasks:
         raise HTTPException(
-            status_code=200,
-            detail={"message": f"Successfully delete task {task_id}"}
+            status_code=404,
+            detail=f"Unable to delete task with ID {task_id}, task not exist"
         )
+    del tasks[task_id]
     raise HTTPException(
-        status_code=404,
-        detail=f"Unable to delete task with ID {task_id}, task not exist"
+        status_code=200,
+        detail={"message": f"Successfully delete task {task_id}"}
     )
+    
 # TODO: write tests for each crud operation in the tasks category
 # TODO: implement a router for tasks
+# TODO: move tasks creation, deletion, update etc to db folder, using db function (even if the db is a dict right now)
